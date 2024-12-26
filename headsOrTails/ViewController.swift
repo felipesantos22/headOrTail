3ee898e0e2e86c27e9020ca2cf01b024ec819176
felipesantos22 <felipevs2018@gmail.com>
2024-12-26 15:18:30 -0300

    Initial Commit

---
+++ headsOrTails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  headsOrTails
+//
+//  Created by Felipe Santos on 26/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
